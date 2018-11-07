42fdd2c8e99473ce2db8cb2d3b59fedc23d79e01
Matthew Collier <collier.matthew@gmail.com>
2018-11-07 13:49:51 -0500

    Add recovery mode tests.

--- test/mocha/010-api.js
+++ test/mocha/010-api.js
@@ -12,7 +12,7 @@ let ledgerNode;
 let electorPoolDocument;
 describe('Elector Selection APIs', () => {
   describe('getBlockElectors API', () => {
-    describe('All electorPool electors are guarantors', () => {
+    describe('Normal operation', () => {
       it('extracts one elector from an electorPool document', async function() {
         this.timeout(60000);
         const electorCount = 0;
@@ -70,8 +70,9 @@ describe('Elector Selection APIs', () => {
         const {electors, recoveryElectors} = r;
         electors.should.be.an('array');
         electors.should.have.length(1);
-        mockData.endpoint.slice(0, 3).should.include(
-          electors.map(({id}) => id)[0]);
+        // the one guarantor should be the chosen elector
+        electors.every(({id}) => mockData.endpoint.slice(0, 1).includes(id))
+          .should.be.true;
         recoveryElectors.should.be.an('array');
         recoveryElectors.should.have.length(1);
         // the recovery elector selected should be the same as the one elector
@@ -102,8 +103,8 @@ describe('Elector Selection APIs', () => {
         const {electors, recoveryElectors} = r;
         electors.should.be.an('array');
         electors.should.have.length(4);
-        mockData.endpoint.slice(0, 4).should.include(
-          electors.map(({id}) => id)[0]);
+        electors.every(({id}) => mockData.endpoint.slice(0, 4).includes(id))
+          .should.be.true;
         recoveryElectors.should.be.an('array');
         recoveryElectors.should.have.length(2);
         // the recovery elector selected should be the same as the one elector
@@ -113,7 +114,7 @@ describe('Elector Selection APIs', () => {
       it('returns 7 electors w/9 in electorPool document', async function() {
         this.timeout(60000);
         const embeddedServiceCount = 9;
-        const guarantorElectorCount = 3;
+        const guarantorElectorCount = 7;
         try {
           const r = await helpers.initializeLedger(
             {embeddedServiceCount, mockData, guarantorElectorCount});
@@ -134,14 +135,83 @@ describe('Elector Selection APIs', () => {
         const {electors, recoveryElectors} = r;
         electors.should.be.an('array');
         electors.should.have.length(7);
-        mockData.endpoint.slice(0, 7).should.include(
-          electors.map(({id}) => id)[0]);
+        // the electors chosen should be the seven guarantors
+        electors.every(({id}) => mockData.endpoint.slice(0, 7).includes(id))
+          .should.be.true;
         recoveryElectors.should.be.an('array');
         recoveryElectors.should.have.length(3);
         // the recovery elector selected should be the same as the one elector
         recoveryElectors.every(({id}) =>
           electors.map(({id}) => id).includes(id)).should.be.true;
       });
-    });
+    }); // end normal operation
+    describe('Recovery Mode', () => {
+      it('returns 1 elector w/3 guarantor electors', async function() {
+        this.timeout(60000);
+        const embeddedServiceCount = 9;
+        const guarantorElectorCount = 3;
+        const recoveryMode = true;
+        try {
+          const r = await helpers.initializeLedger(
+            {embeddedServiceCount, mockData, guarantorElectorCount});
+          ledgerNode = r.ledgerNode;
+          electorPoolDocument = r.electorPoolDocument;
+        } catch(err) {
+          assertNoError(err);
+        }
+        const ledgerConfiguration = await ledgerNode.config.get();
+        const latestBlockSummary = await ledgerNode.blocks.getLatestSummary();
+        const blockHeight = latestBlockSummary.eventBlock.block.blockHeight + 1;
+        const r = await es.getBlockElectors({
+          blockHeight, latestBlockSummary, ledgerConfiguration, ledgerNode,
+          recoveryMode
+        });
+        should.exist(r);
+        r.should.be.an('object');
+        r.should.have.property('electors');
+        r.should.have.property('recoveryElectors');
+        const {electors, recoveryElectors} = r;
+        electors.should.be.an('array');
+        electors.should.have.length(1);
+        // the chosen elector should be one of the three guarantors
+        electors.every(({id}) => mockData.endpoint.slice(0, 3).includes(id))
+          .should.be.true;
+        recoveryElectors.should.be.an('array');
+        recoveryElectors.should.have.length(0);
+      });
+      it('returns 4 electors w/4 guarantor electors', async function() {
+        this.timeout(60000);
+        const embeddedServiceCount = 9;
+        const guarantorElectorCount = 4;
+        const recoveryMode = true;
+        try {
+          const r = await helpers.initializeLedger(
+            {embeddedServiceCount, mockData, guarantorElectorCount});
+          ledgerNode = r.ledgerNode;
+          electorPoolDocument = r.electorPoolDocument;
+        } catch(err) {
+          assertNoError(err);
+        }
+        const ledgerConfiguration = await ledgerNode.config.get();
+        const latestBlockSummary = await ledgerNode.blocks.getLatestSummary();
+        const blockHeight = latestBlockSummary.eventBlock.block.blockHeight + 1;
+        const r = await es.getBlockElectors({
+          blockHeight, latestBlockSummary, ledgerConfiguration, ledgerNode,
+          recoveryMode
+        });
+        should.exist(r);
+        r.should.be.an('object');
+        r.should.have.property('electors');
+        r.should.have.property('recoveryElectors');
+        const {electors, recoveryElectors} = r;
+        electors.should.be.an('array');
+        electors.should.have.length(4);
+        // the chosen elector should be one of the four guarantors
+        electors.every(({id}) => mockData.endpoint.slice(0, 4).includes(id))
+          .should.be.true;
+        recoveryElectors.should.be.an('array');
+        recoveryElectors.should.have.length(0);
+      });
+    }); // end recovery mode
   });
 });
